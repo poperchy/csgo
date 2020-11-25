9bc44ba7151c961d376eab502aaa5dd884eedd98
dik <makkarevi4@gmail.com>
2020-11-25 22:49:22 +0300

    йцйц

--- src/app/js/App.js
+++ src/app/js/App.js
@@ -1,4 +1,34 @@
+
 window.onload = function () {
+
+    var burger = document.querySelector('.burger');
+    var close = document.querySelector('.close');
+    var navMenu = document.querySelector('.nav__mobile');
+    var bodyL = document.querySelector('body');
+    var mobLink = document.querySelectorAll('.mobile--link');
+    mobLink.forEach(function(item) {
+        item.addEventListener('click', function(){
+            navMenu.classList.remove('nav__mobile--active')
+            bodyL.style.overflow = "auto";
+            bodyL.style.overflowX = "hidden";
+
+        })
+    });
+
+
+    burger.addEventListener('click', function(){
+        navMenu.classList.add('nav__mobile--active');
+        bodyL.style.overflow = "hidden";
+    });
+    close.addEventListener('click', function(){
+        navMenu.classList.remove('nav__mobile--active')
+        bodyL.style.overflow = "auto";
+        bodyL.style.overflowX = "hidden";
+    });
+
+
+
+
     var cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
 
     const body = document.querySelector('body')
@@ -26,6 +56,8 @@ window.onload = function () {
         gsap.to('.ak', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
         gsap.to('.boom', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
         gsap.to('.glock', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
+        gsap.to('.coins', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
+        gsap.to('.s', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
 
 
     }
@@ -48,6 +80,7 @@ $(document).ready(function () {
 
     var show = true;
     var countbox = ".indicator__wrap";
+    var countbox2 = ".our__item";
     $(window).on("scroll load resize", function () {
         if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
         var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
@@ -55,11 +88,12 @@ $(document).ready(function () {
         var w_height = $(window).height(); // Высота окна браузера
         var d_height = $(document).height(); // Высота всего документа
         var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+        var e_height2 = $(countbox2).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height || e_height2 + e_top < w_height) {
             $('.dynamic-number').css('opacity', '1');
             $('.dynamic-number').spincrement({
                 thousandSeparator: "",
-                duration: 1200
+                duration: 2500
             });
 
             show = false;
@@ -68,11 +102,34 @@ $(document).ready(function () {
 
 });
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
+    spaceBetween: 50,
+    centeredSlides: true,
+    roundLengths: true,
+    slidesPerView: 'auto',
+    loopAdditionalSlides: 30,
+    initialSlide: 1,
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
+        type: 'fraction',
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
 });
+
+$('a[href^="#"]').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
+
+
+AOS.init();
